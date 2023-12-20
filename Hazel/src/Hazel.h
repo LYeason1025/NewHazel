00d3ec492edaff5fb6706cde13c53fd02266adf4
LYeason1025 <321682959@qq.com>
2023-12-20 14:19:34 +0800

    SetUp Basic Application And Entrypoint

---
+++ Hazel/src/Hazel.h
@@ -0,0 +1,9 @@
+#pragma once
+// hazel application 
+#include "Hazel/Application.h"
+
+#include <iostream>
+
+//Entrypoint
+#include "Hazel/Entrypoint.h"
+
---
+++ Sandbox/src/SandboxApp.cpp
@@ -0,0 +1,28 @@
+#include "Hazel.h"
+
+class Sandbox:public Hazel::Application
+{
+public:
+	 Sandbox();
+	~ Sandbox();
+
+private:
+
+};
+
+
+
+ Sandbox:: Sandbox()
+{
+}
+
+ Sandbox::~ Sandbox()
+{
+}
+
+ Hazel::Application* Hazel::CreateApplication() {
+	 return new Sandbox();
+ }
+
+
+
